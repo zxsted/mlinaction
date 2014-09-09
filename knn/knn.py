f77ad76b9d0f05ea53e5a3a1f961990f781a357b
zxsted <zxsted@zxsted-ThinkPad-Edge-E430c.(none)>
2014-09-09 09:31:39 +0800

    add handwritingclasstest

--- knn/knn.py
+++ knn/knn.py
@@ -4,6 +4,8 @@
 from numpy import *
 import operator
 import numpy as np
+import os
+
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -71,15 +73,82 @@ def data2graph(dataMat,labels,index):
 
 
 #归一化特征值
-def autoNorm(dataset):
+def autoNorm(dataSet):
 	minVals = dataSet.min(0)
 	maxVals = dataSet.max(0)
 	ranges = maxVals - minVals
 	normDataSet = zeros(shape(dataSet))
+	m = dataSet.shape[0]
 	normDataSet = dataSet - tile(minVals,(m,1))
 	normDataSet = normDataSet/tile(ranges,(m,1))
 	return normDataSet,ranges,minVals
 
 
 
+#定义测试函数，这里将数据集随机的划分为90% 和10%的两个子数据集，
+#分别作为训练集和测试集
+def datingClassTest():
+	hoRatio = 0.10
+	datingDataMat,datingLabels = file2matrix('../data/datingTestSet2.txt')
+	normMat,ranges,minVals = autoNorm(datingDataMat)
+	m = normMat.shape[0]
+	numTestVecs = int(m*hoRatio)
+	errorCount = 0.0
+	for i in range(numTestVecs):
+		classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],
+				datingLabels[numTestVecs:m],3)
+		print "the classifier come bacl with： %d,the real answer is %d" \
+				%(classifierResult,datingLabels[i])
+		if (classifierResult != datingLabels[i]):
+			errorCount += 1
+	print "the total error rate is: %f" % (errorCount/float(numTestVecs))
 
+
+
+
+
+###########################################################################
+#下面是手写数字的knn应用
+
+#将图像转换为向量
+def img2vector(filename):
+	returnvect = zeros((1,1024))
+	fr = open(filename)
+	for i in range(32):
+		linestr = fr.readline()
+		for j in range(32):
+			returnVect[0,32j+1] = int[lineStr[j]]
+	return returnVect
+
+
+#下面是手写数据的测试代码
+def handwritingClassTest():
+	hwLabels = []
+	#获取目录内容
+	trainFileList = os.listdir('../data/trainingDigits')
+	m = len(trainingFileList)
+	trainingMat = zeros((m,1024))
+	for i in range(m):
+		#从文件名解析分类数字
+		fileNameStr = trainingFileList[i]
+		fileStr = fileNameStr.split('.')[0]
+		classNumStr = int(fileStr.split('_')[0])
+		hwLabels.append(classNumStr)
+		trainingMat[i,:] = img2vector('../data/trainingDigits/%s' % fileNameStr)
+
+	testFileList = listdir('../data/testDigists')
+	errorCount = 0.0
+	mTest = len(testFileList)
+	for i in range(mTest):
+		fileNameStr = testFileList[i]
+		fileStr = fileNameStr.split('.')[0]
+		classNumStr = int(fileStr.split('_')[0])
+		vectorUnderTest = img2rvctor('../data/testDigits/%s' % fileNameStr)
+		classifierResult = classify0(vectorUnderTest,\
+				trainingMat,heLabels,3)
+		print 'the classifier came back with: %d ,the real answer is : %d' \
+				%(classifierResult,classNumStr)
+		if classifierResult != classNumStr: 
+			errornum += 1
+	print "\n the total number of errors is : %d" % errorCount
+	print "\n the total rate is : %f" %(errorCount/float(mTest))
